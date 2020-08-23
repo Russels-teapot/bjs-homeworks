b0c66e6769de7267325a5b676746d35c79d3c1f5
Russels-teapot <baxarev.777@yandex.ru>
2020-08-23 16:24:45 +0300

    Выполнена домашняя работа

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,44 @@
+"use strict"
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    let D = Math.pow(b, 2) - 4*a*c;
+    if (D < 0)
+        x = [];
+    if (D > 0)
+        x.push((Math.sqrt(D) - b) / 2*a)
+        x.push((- Math.sqrt(D) - b) / 2*a);
+    if (D === 0)
+        x.push(-b / 2*a);
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    if (marks.length === 0)
+        return 0;
+    if (marks.length > 5)
+        console.log("Количество оценок больше пяти")
+    let averageMark;
+    let mark;
+    let sumOfMarks = 0;
+    let newMarks = marks.slice(0, 4)
+    for (mark of newMarks) {
+        sumOfMarks += mark
+    }
+    averageMark = sumOfMarks / newMarks.length
+    return +averageMark.toFixed(2)
+
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let yearOfBirth = dateOfBirthday.getFullYear();
+    let today = new Date();
+    let currentYear = today.getFullYear();
+    let message;
+    if ((currentYear - yearOfBirth) >= 18)
+        message = `Не желаете ли олд-фэшн, ${name}?`
+    else message = `Сожалею, ${name}, но я не могу вам продать алкоголь. 
+        Зато могу предложить вам замечательный клюквенный компот!`
+    return message
+
 }
