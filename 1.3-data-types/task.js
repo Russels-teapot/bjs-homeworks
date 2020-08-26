27d1f691322140c2d3a8218c144f2f1764696de9
Russels-teapot <baxarev.777@yandex.ru>
2020-08-26 20:26:03 +0300

    Выполнена домашняя работа

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,21 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  let mortgageBody = amount - contribution;
+  let finishDate = new Date(date);
+  let quantityOfMonths = finishDate.getFullYear()*12 + finishDate.getMonth()
+      - (new Date().getFullYear()*12 + new Date().getMonth());
+  let monthlyPercent = percent/12;
+  let monthlyPayment = mortgageBody*(monthlyPercent + monthlyPercent/(((1+ monthlyPercent)^quantityOfMonths) - 1));
+  let totalAmount = (monthlyPayment*quantityOfMonths + contribution).toFixed(2);
+  return totalAmount;
+  console.log(finishDate);
+  console.log(totalAmount);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if (name)
+    greeting = `Привет, мир! Меня зовут ${name}`;
+    else greeting = 'Привет, мир! Меня зовут Аноним';
+    return greeting;
 }
