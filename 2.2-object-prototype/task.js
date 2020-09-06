a0208d3e9e53c276424423f4185b6822de5d6b31
Russels-teapot <baxarev.777@yandex.ru>
2020-09-06 15:39:11 +0300

    Выполнена домашняя работа

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,29 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    strNorm = this.toLowerCase().replace(/\s/g, '');
+    strRev = strNorm.split('').reverse().join('');
+    if (strRev === strNorm)
+        return true;
+    else return false
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sumOfMarks = 0;
+    for (let mark of marks) {
+        sumOfMarks+=mark
+    }
+    let averageMark;
+    if (marks.length === 0)
+        return 0;
+    else return averageMark = Math.round(sumOfMarks/marks.length)
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    let date = new Date(birthday).getTime();
+    let diff = now - date;
+    let age = diff / 31557600000;
+    if (age >= 18)
+        return true;
+    else return false
 }
